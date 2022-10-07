481189cf02b357177c4406bbd54af82fc125de43
Jayesh02032000 <106960747+Jayesh02032000@users.noreply.github.com>
2022-10-07 19:11:34 +0530

    MongoDB changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB");
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
 
 const postSchema = {
   title: String,
